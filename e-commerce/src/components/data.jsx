13a7d3b5f800260a52d656a2ac67afbddcd56ba0
Hector Montserrat <h.montserratm@gmail.com>
2023-11-07 16:44:55 -0400

    iniciando

---
+++ e-commerce/src/components/data.jsx
@@ -0,0 +1,16 @@
+const data = {
+    products: [
+        {
+        name: 'Nike Slim Shirt',
+        slug: 'nike-slim-shirt',
+        category : 'Shirts',
+        image : '/images/imagen1.webp',
+        price : 120,
+        countInStock : 10,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews: 10,
+        description: 'high quality shirt',
+        },
+    ],
+};
